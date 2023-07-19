57aed1ea96f55664c80150d72b73c33385594101
DawnforWorld <QQ1739770884@163.com>
2023-07-20 00:27:15 +0800

    update .gitignore

---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include "check_gl.hpp" // includes glad/glad.h
+#include <GLFW/glfw3.h> // must be placed behind glad/glad.h
+#include <stdexcept>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+static void render() {
+   
+}
+
+int main() {
+    if (!glfwInit()) {
+        const char *errmsg;
+        glfwGetError(&errmsg);
+        if (!errmsg) errmsg = "(no error)";
+        std::cerr << "failed to initialize GLFW: " << errmsg << '\n';
+        return -1;
+    }
+
+    // hint the version required: OpenGL 4.5
+    constexpr int version = 45;
+    glfwWindowHint(GLFW_OPENGL_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, version / 10);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, version % 10);
+    if (version >= 33) {
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    }
+
+    // Create window
+    GLFWwindow *window = glfwCreateWindow(640, 640, "Example", NULL, NULL);
+    if (!window) {
+        const char *errmsg;
+        glfwGetError(&errmsg);
+        if (!errmsg) errmsg = "(no error)";
+        std::cerr << "GLFW failed to create window: " << errmsg << '\n';
+        std::cerr << "==============================================\n";
+        if (!strcmp(errmsg, "X11: The DISPLAY environment variable is missing")) {
+            std::cerr << "You seems not running with graphic display\n";
+        } else if (!strcmp(errmsg, "WGL: The driver does not appear to support OpenGL")) {
+            std::cerr << "Please consider install an OpenGL driver, or use the mesa driver\n";
+        } else if (!strcmp(errmsg, "WGL: Failed to create OpenGL context")) {
+            std::cerr << "Your driver seems not supporting the required OpenGL version\n";
+        }
+        std::cerr << "- If you have a physical graphic card (e.g. NVIDIA), install it from your graphic card vendor official website: http://www.nvidia.com/Download/index.aspx\n";
+        std::cerr << "- If you are using Windows, download opengl32.dll from https://pan.baidu.com/s/1TZ6nVJC7DZIuUarZrGJYow?pwd=opgl and place it into the same directory as this executable file (alternatively you may download opengl32sw.dll from Internet and rename it to opengl32.dll to place into the same directory as this executable file)\n";
+        std::cerr << "- If you are using Linux or WSL1, install the mesa driver: https://ubuntuhandbook.org/index.php/2021/07/install-latest-mesa-ubuntu-20-04-21-04/";
+        std::cerr << "- If you use WSL2, install WSLg: https://learn.microsoft.com/zh-cn/windows/wsl/tutorials/gui-apps\n";
+        std::cerr << "- If you are using SSH remote server, try connect it using ssh -X <ip address>\n";
+        std::cerr << "- If you are using MacOS, you probably want to use Windows or Linux instead for better OpenGL support\n";
+        std::cerr << "- If you are using a Laptop with dual-cards, make sure you have switch to dedicated card (NVIDIA) instead of the integrated card (Intel)\n";
+        std::cerr << "==============================================\n";
+#ifdef _WIN32
+        std::system("pause");
+#endif
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Load glXXX function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        glfwTerminate();
+        std::cerr << "GLAD failed to load GL functions\n";
+        return -1;
+    }
+    std::cerr << "OpenGL version: " << glGetString(GL_VERSION) << '\n';
+    
+  
+    // start main game loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // render graphics
+        glClearColor(0.2f, 0.3f, 0.2f, 1.0f);
+        CHECK_GL(glClear(GL_COLOR_BUFFER_BIT));
+
+        render();
+        // refresh screen
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
