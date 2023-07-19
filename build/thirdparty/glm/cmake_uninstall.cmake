18a975f4d5a8fb8ce40b686b634e632053bb9748
DawnforWorld <QQ1739770884@163.com>
2023-07-20 00:32:59 +0800

    update .gitignore

---
+++ build/thirdparty/glm/cmake_uninstall.cmake
@@ -0,0 +1,21 @@
+if(NOT EXISTS "E:/opengl_breakout_2D/build/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: E:/opengl_breakout_2D/build/install_manifest.txt")
+endif()
+
+file(READ "E:/opengl_breakout_2D/build/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+foreach(file ${files})
+  message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
+  if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    exec_program(
+      "D:/cmake-3.26.4-windows-x86_64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+      OUTPUT_VARIABLE rm_out
+      RETURN_VALUE rm_retval
+      )
+    if(NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${file}")
+    endif()
+  else(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    message(STATUS "File $ENV{DESTDIR}${file} does not exist.")
+  endif()
+endforeach()
