28140bf8965a8d6ae85728774787db9718998bdc
枫 秦 <1753372894@qq.com>
2023-07-22 18:34:56 +0800

    7.22

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,24 @@
 #include "game.h"
 
-game::game() {
+game::game(GLuint Width, GLuint Height) :State(GAME_ACTIVE), Key(), Width(width), Height(height) {
 
 }
 
 game::~game() {
+
+}
+void game::Init() {
+
+}
+
+void game::Update(GLfloat dt) {
+
+}
+
+void game::ProcessInput(GLfloat dt) {
+
+}
+
+void game::Render() {
 
 }
