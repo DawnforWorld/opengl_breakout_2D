44eb7dba67caa62cfdbad3842918555f92be708c
枫 秦 <1753372894@qq.com>
2023-08-02 17:15:24 +0800

    08.02.1

--- src/ball_object.cpp
+++ src/ball_object.cpp
@@ -42,4 +42,6 @@ void BallObject::Reset(glm::vec2 position, glm::vec2 velocity)
     this->Position = position;
     this->Velocity = velocity;
     this->Stuck = true;
+    this->Sticky = false;
+    this->PassThrough = false;
 }
